2b711f993a94d41176d8dc942cc3f9d5199d2f83
minhgtr25 <dquangminh0103@gmail.com>
2024-12-27 11:24:02 +0700

    edited

--- script.js
+++ script.js
@@ -23,3 +23,40 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(element);
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyObJNgAOusW-GSiiGCkaMdXck1rlHGJS8p8hVV0WZKsc9tThXXzf6Mso2joGQgdWPa/exec';
+    const form = document.forms['contact-form'];
+
+    if (!form) {
+        console.error('Form not found');
+        return;
+    }
+
+    form.addEventListener('submit', e => {
+        e.preventDefault(); // Ngăn chặn hành vi mặc định của form
+
+        // Hiển thị trạng thái gửi
+        const submitButton = form.querySelector('button[type="submit"]');
+        submitButton.textContent = "Đang gửi...";
+        submitButton.disabled = true;
+
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                if (response.ok) {
+                    alert("Gửi thành công. Cảm ơn bạn vì đã lựa chọn SmartPath!");
+                    form.reset(); // Reset form về trạng thái ban đầu
+                } else {    
+                    alert("Ôi có lỗi mất rồi :( Hãy gửi lại nhé.");
+                }
+            })
+            .catch(error => {
+                console.error('Error!', error.message);
+                alert("Lỗi gửi đơn. Vui lòng kiểm tra lại kết nối Internet.");
+            })
+            .finally(() => {
+                submitButton.textContent = "Submit";
+                submitButton.disabled = false;
+            });
+    });
+});
